4cf751f45bffc1e10b944907bed62572e26506e6
Rishabh Mishra <rish3ha@gmail.com>
2024-11-27 00:51:41 -0800

    added update and delete routes

--- backend/routes/Products.js
+++ backend/routes/Products.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import {CreateProducts, UpdateProduct} from '../controllers/Products.js'
+import {CreateProducts, UpdateProduct, DeleteProduct} from '../controllers/Products.js'
 
 const ProductRoutes = express.Router()
 
 ProductRoutes.post('/create/:userID', CreateProducts)
-ProductRoutes.put('/update', UpdateProduct)
+ProductRoutes.put('/update/:id', UpdateProduct)
+ProductRoutes.delete('/delete/:id', DeleteProduct)
 
 export default ProductRoutes
